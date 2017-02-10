f163d98268eccdd2c709ddd552c9854626f9a874
Shawn O'Connor <bartandfosto@gmail.com>
2017-02-10 16:26:58 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Custion method written in JavaScript for flattening nested arrays of varying depth.
---
+++ spec/jasmine_examples/FlattenSpec.js
@@ -0,0 +1,19 @@
+describe("Flatten", function() {
+  var flatten = require('../../lib/flattenArray.js');
+  var flatten;
+
+  beforeEach(function() {
+    exampleArray = [0, 2, [[2, 3], 8, 100, null, [[null]]], -2];
+  });
+
+  it("should not contain any null values", function() {
+    
+  });
+
+  
+
+  // demonstrates use of spies to intercept and test method calls
+  it("should only contain numbers", function() {
+    
+  });
+});
