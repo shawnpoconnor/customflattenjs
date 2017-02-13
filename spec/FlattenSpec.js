087681e91b89675f23eb6f95d915de0f40c88ec6
Shawn O'Connor <bartandfosto@gmail.com>
2017-02-13 02:53:52 -0500

    change flatten function to be a prototype of the Array object and add one test to check deeply nested arrays

--- spec/FlattenSpec.js
+++ spec/FlattenSpec.js
@@ -1,13 +1,15 @@
 describe("Flatten", function() {
-  var flatten = require('../lib/flattenArray.js');
-  var flatten;
+  require('../lib/flattenArray.js');
 
   beforeEach(function() {
     exampleArray = [0, 2, [[2, 3], 8, 100, null, [[null]]], -2];
+    nestedArray = [[[[[[[[[[[[[[[[[[[[[[[1, [2, 3]]]]]]]]]]]]]]]]]]]]]]]];
     nullArray = [null];
   });
 
   it("should not contain any null values", function() {
+    
+
     nullArray = nullArray.flatten();
 
       var containsNull = false;
@@ -20,11 +22,26 @@ describe("Flatten", function() {
       expect(containsNull).toEqual(false);
   });
 
+  it("should remove nested null values", function() {
+    
+
+    exampleArray = exampleArray.flatten();
+
+      var containsNull = false;
+      for (var i = exampleArray.length - 1; i >= 0; i--) {
+        if(exampleArray[i] === null) {
+          containsNull = true;
+        }
+      }
+
+      expect(containsNull).toEqual(false);
+  });
+
   
 
-  // demonstrates use of spies to intercept and test method calls
   it("should not contain any nested arrays", function() {
-    exampleArray = flatten(exampleArray);
+
+    exampleArray = exampleArray.flatten();
 
     var containsArray = false;
     for (var i = exampleArray.length - 1; i >= 0; i--) {
@@ -34,6 +51,21 @@ describe("Flatten", function() {
     }
 
     expect(containsArray).toEqual(false);
+  });
+
+  it("Can flatten very nested values", function() {
+
+    nestedArray = nestedArray.flatten();
+
+    var containsArray = false;
+    for (var i = exampleArray.length - 1; i >= 0; i--) {
+      if(Array.isArray(exampleArray[i])) {
+        containsArray = true;
+      }
+    }
+
+    expect(containsArray = false);
+
   });
 
 });
